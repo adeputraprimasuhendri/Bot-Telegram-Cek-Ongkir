afd96cc6529a6fe2b84d20777696eb85556e7add
FIAND T <alfianvega@outlook.com>
2018-11-05 07:41:06 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+debug_backtrace() || die ("<h2> Akses Ditolak! </ h2> Anda tidak dapat mengakses file ini secara langsung.");
+
+session_start();
+
+define("DB", [
+    "DB_HOST_NAME" => "localhost",
+    "DB_USER_NAME" => "root",
+    "DB_PASSWORD"  => "root",
+    "DB_DATABASE"  => "dbname"
+]);
+
+define("ONGKIR", [
+    "RAJAONGKIR_KEY"       => "your_api_key",
+    "RAJAONGKIR_AKUN_TIPE" => "starter",
+    "TELEGRAM_BOT_TOKEN"   => "your:bot_token",
+]);
+
+define("SITE_CONF", [
+    "DEBUG"      => TRUE
+]);
+
+SITE_CONF['DEBUG'] == TRUE ? error_reporting(E_ALL) : error_reporting(0);
+
+
+spl_autoload_register(function($class) {
+    require_once 'classes/class.' . $class . '.php';
+});
+
